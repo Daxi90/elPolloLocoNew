9ba2ac17469080be184df9807fefc58c6420e476
Daxi90 <hdateam90@gmail.com>
2023-08-27 13:57:14 +0200

    Added first classes

---
+++ js/game.js
@@ -0,0 +1,11 @@
+let canvas;
+let world = new World();
+
+
+function init(){
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    console.log('My character is', world.character);
+    
+}
---
+++ models/movable-object.class.js
@@ -0,0 +1,18 @@
+class MovableObject{
+    x = 120;
+    y = 140;
+    img;
+
+    loadImage(path){
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    moveRight(){
+        console.log('Moving right');
+    }
+
+    moveLeft(){
+        
+    }
+}
