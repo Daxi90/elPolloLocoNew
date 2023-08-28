b7e2a4d36ce614c217fc58d02294cbdf39f57780
Daxi90 <hdateam90@gmail.com>
2023-08-28 08:12:53 +0200

    Added requestanimationframe

--- js/game.js
+++ js/game.js
@@ -1,11 +1,8 @@
 let canvas;
-let world = new World();
+let world;
 
 
 function init(){
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-
-    console.log('My character is', world.character);
-    
+    world = new World(canvas);    
 }
