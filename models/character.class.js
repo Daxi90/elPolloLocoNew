09136a65c3430ec85de6821e58ce3c9111d42d83
Daxi90 <hdateam90@gmail.com>
2023-09-01 08:49:01 +0200

    Animate character only on keypress right

--- models/character.class.js
+++ models/character.class.js
@@ -1,35 +1,32 @@
 class Character extends MovableObject {
+  IMAGES_WALKING = [
+    "../img/2_character_pepe/2_walk/W-21.png",
+    "../img/2_character_pepe/2_walk/W-22.png",
+    "../img/2_character_pepe/2_walk/W-23.png",
+    "../img/2_character_pepe/2_walk/W-24.png",
+    "../img/2_character_pepe/2_walk/W-25.png",
+    "../img/2_character_pepe/2_walk/W-26.png",
+  ];
+  world;
 
-    IMAGES_WALKING = [
-        '../img/2_character_pepe/2_walk/W-21.png',
-        '../img/2_character_pepe/2_walk/W-22.png',
-        '../img/2_character_pepe/2_walk/W-23.png',
-        '../img/2_character_pepe/2_walk/W-24.png',
-        '../img/2_character_pepe/2_walk/W-25.png',
-        '../img/2_character_pepe/2_walk/W-26.png'
-    ]
-    world;
+  constructor() {
+    super().loadImage("../img/2_character_pepe/2_walk/W-21.png");
+    this.loadImages(this.IMAGES_WALKING);
 
+    this.animate();
+  }
 
-    constructor(){
-        super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages(this.IMAGES_WALKING);
+  jump() {}
 
-        this.animate();
-    }
+  animate() {
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; Rest 0
 
-    jump(){
-
-    }
-
-    animate(){
-        setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; Rest 0
-
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;            
-        }, 100);
-        
-    }
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 100);
+  }
 }
