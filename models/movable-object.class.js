62175d467c96a303ce850a86be96b8dcfd8efd10
Daxi90 <hdateam90@gmail.com>
2023-09-02 12:48:04 +0200

    Added emdboss

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -43,4 +43,12 @@ class MovableObject{
         }, 1000/60);
 
     }
+
+    playAnimation(images){
+        let i = this.currentImage % images.length; // let i = 0 % 6; Rest 0
+
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;   
+    }
 }
