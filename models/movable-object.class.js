aeebfda71a2b078c2bd44bbf301ce9fef7d98fb1
Daxi90 <hdateam90@gmail.com>
2023-09-03 10:24:45 +0200

    Refactored code

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -45,6 +45,19 @@ class MovableObject{
 
     }
 
+    draw(ctx){
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx){
+        ctx.beginPath();
+        ctx.lineWidth = "5";
+        ctx.strokeStyle = "blue";
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
+
+
     moveRight(){
         this.x += this.speed;
         this.walking_sound.play();
